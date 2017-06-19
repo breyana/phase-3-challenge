48018cda8d24e4b52ea86d36af920e16cb137b53
Breyana Scales <breyanaj@gmail.com>
2017-06-19 11:47:17 -0700

    Add add route

--- part-1/index.js
+++ part-1/index.js
@@ -3,6 +3,11 @@ const app = express()
 
 app.get('/zero', (req, res) => res.send('0'))
 
+app.get('/add', (req, res) => {
+  const total = +req.query.a + +req.query.b
+  res.send(total.toString())
+})
+
 app.listen(3000, function () {
   console.log('App listening on port 3000!')
 })
