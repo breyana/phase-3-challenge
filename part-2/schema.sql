af2dd3bca4a9e9f012da9f240b0b669fcc39df42
Breyana Scales <breyanaj@gmail.com>
2017-06-19 15:48:38 -0700

    Add schema tables grocery items, shoppers, orders, and ordered_items

--- part-2/schema.sql
+++ part-2/schema.sql
@@ -1,1 +1,11 @@
+DROP TABLE IF EXISTS grocery_items;
+DROP TABLE IF EXISTS shoppers;
+DROP TABLE IF EXISTS orders;
 
+CREATE TABLE grocery_items (id INTEGER PRIMARY KEY, name VARCHAR(255), price DECIMAL, section VARCHAR(255));
+
+CREATE TABLE shoppers (id INTEGER PRIMARY KEY, name VARCHAR(255));
+
+CREATE TABLE orders (id INTEGER PRIMARY KEY, order_date DATE, shopper_id INTEGER);
+
+CREATE TABLE ordered_items (order_id INTEGER, grocery_id INTEGER);
