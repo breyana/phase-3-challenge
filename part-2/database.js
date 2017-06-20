996eee7732533ee772242cd87620ba840b9d1ff0
Breyana Scales <breyanaj@gmail.com>
2017-06-20 08:53:28 -0700

    Add console logging for data and errors

--- part-2/database.js
+++ part-2/database.js
@@ -8,8 +8,25 @@ const db = pgp(connection)
 
 const allItems = () => {
   return db.any('SELECT * FROM grocery_items')
+    .then(data => {
+      console.log('DATA:', data);
+    })
+    .catch(error => {
+      console.log('ERROR:', error);
+    })
 }
 
 const itemsInSection  = (section) => {
   return db.any('SELECT * FROM grocery_items WHERE section = $1', [section])
+    .then(data => {
+      console.log('DATA:', data);
+    })
+    .catch(error => {
+      console.log('ERROR:', error);
+    })
+}
+
+module.exports = {
+  allItems,
+  itemsInSection
 }
