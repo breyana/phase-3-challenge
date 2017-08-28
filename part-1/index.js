c2cb27207ae858edc22be976be9d4d9e62536e37
Breyana Scales <breyanaj@gmail.com>
2017-08-28 09:37:36 -0700

    Add concat API

--- part-1/index.js
+++ part-1/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 app.get('/api/days/:day', (request, response) => {
   const day = request.params.day
@@ -17,6 +20,16 @@ app.get('/api/days/:day', (request, response) => {
     response.status(200).send(`${daysOfWeek[day]}`)
   } else {
     response.status(404).send(`'${day}' is not a valid day!`)
+  }
+})
+
+app.post('/api/array/concat', (request, response) => {
+  const array1 = request.body.array1
+  const array2 = request.body.array2
+  if (Array.isArray(array1) && Array.isArray(array2)) {
+    response.json({"result": array1.concat(array2)})
+  } else {
+    response.status(400).json({"error": "Input data should be of type Array."})
   }
 })
 
