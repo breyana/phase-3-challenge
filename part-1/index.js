977d38c2a9580e4718b068bd5045d61845f1ae7f
Breyana Scales <breyanaj@gmail.com>
2017-08-28 09:05:05 -0700

    Add Day API

--- part-1/index.js
+++ part-1/index.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const app = express()
 
+app.get('/api/days/:day', (request, response) => {
+  const day = request.params.day
+  const daysOfWeek = {
+    monday: 1,
+    tuesday: 2,
+    wednesday: 3,
+    thursday: 4,
+    friday: 5,
+    saturday: 6,
+    sunday: 7
+  }
+
+  if (daysOfWeek[day]) {
+    response.status(200).send(`${daysOfWeek[day]}`)
+  } else {
+    response.status(404).send(`'${day}' is not a valid day!`)
+  }
+})
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!')
