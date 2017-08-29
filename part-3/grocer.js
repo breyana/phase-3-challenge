9291f83cf64ce79c43bac2ef3ecc9d3eb87059a4
Breyana Scales <breyanaj@gmail.com>
2017-08-29 10:41:28 -0700

    Implement adding new items to the cart

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,6 +1,8 @@
 const modal = document.querySelector('.modal')
 const cartButton = document.querySelector('#cart-button')
 const x = document.querySelector('.close')
+const addToCartButtons = document.querySelectorAll('.item button')
+const cartContents = document.querySelector('.cart-contents')
 
 cartButton.addEventListener('click', function() {
   modal.style.display = 'block'
@@ -8,4 +10,25 @@ cartButton.addEventListener('click', function() {
 
 x.addEventListener('click', function() {
   modal.style.display = 'none'
+})
+
+addToCartButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    const newListItem = document.createElement('li')
+    newListItem.className = 'item flex flex-row-between'
+
+    const item = event.target.previousElementSibling.previousElementSibling.innerText
+    const itemSpan = document.createElement('span')
+    itemSpan.innerText = item
+    itemSpan.className = 'item-name'
+    newListItem.appendChild(itemSpan)
+
+    const price = event.target.previousElementSibling.innerText
+    const priceSpan = document.createElement('span')
+    priceSpan.innerText = price
+    priceSpan.className = 'item-price'
+    newListItem.appendChild(priceSpan)
+
+    cartContents.appendChild(newListItem)  
+  })
 })
